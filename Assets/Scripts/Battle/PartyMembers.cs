b34ac6f09198f85dcc132d5ddea26e08c2612612
Zary <imzarylive2019@gmail.com>
2023-07-05 16:33:13 +0100

    a lot of changes & the start of the battle systems

---
+++ Assets/Scripts/Battle/PartyMembers.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+public class PartyMembers : MonoBehaviour
+{
+    public string memberName;
+    public int memberDefense;
+    public int memberAttack;
+    public int maxHp;
+    public int currentHp;
+    public bool isDead;
+}
